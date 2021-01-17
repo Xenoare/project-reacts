57ba2c9bc4ac702b1ee3eaddb9f3b67b3c17273d
Xenoare <xenoare@gmail.com>
2021-01-17 12:04:28 +0700

    Latihan Ke-1

--- 1-birthday-reminder/setup/src/List.js
+++ 1-birthday-reminder/setup/src/List.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-const List = () => {
+const List = ({people}) => {
+  console.log(people)
   return (
     <>
-      <h2>list component</h2>
+      {people.map((person) => {
+    const {id,name,age,image} = person
+    return(
+      <article key={id} className="person">
+        <img src={image} alt=""/>
+        <div>
+          <h3>{name}</h3>
+          <p>{age}</p>
+        </div>
+      </article>
+    )
+  })}
     </>
   );
 };
