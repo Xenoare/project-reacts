5865c91055a0ea0d450ac563d56b34cc6086faa4
Xenoare <xenoare@gmail.com>
2021-01-22 11:30:43 +0700

    -git commit -am "Project-2"

--- 2-tours/setup/src/App.js
+++ 2-tours/setup/src/App.js
@@ -5,7 +5,54 @@ import Tours from './Tours'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading,setLoading] = useState(true)
+  const [tours,setTours] = useState([])
+
+  const removeTour = (id) => {
+    const newTour = tours.filter((tour) => tour.id !== id)
+    setTours(newTour)
+  }
+
+  const fetchTours = async() =>{
+    setLoading(true)
+    try{
+    const response = await fetch(url)
+    const tours = await response.json()
+    setLoading(false)
+    setTours(tours)
+    }
+    catch(error){
+    setLoading(false)
+    console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchTours()
+  },[])
+
+  if(loading){
+    return(
+      <main>
+      <Loading />
+      </main>
+    )
+  }
+
+  if(tours.length === 0){
+    return(
+      <main>
+        <h4>no one left</h4>
+        <button onClick={() => fetchTours()}>Refresh</button>
+      </main>
+    )
+  }
+  
+  return(
+  <main>
+    <Tours tours={tours} removeTour={removeTour} />
+  </main>
+  )
 }
 
 export default App
--- 2-tours/setup/src/Tour.js
+++ 2-tours/setup/src/Tour.js
@@ -1,7 +1,22 @@
 import React, { useState } from 'react';
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({id,image,info,name,price,removeTour}) => 
+{
+  const [readMore,setReadMore] = useState(false)
+  return (
+    <article className="single-tour">
+        <img src={image} alt={name}/>
+        <footer className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">${price}</h4>
+        </footer>
+        <p>{readMore ? info : `${info.substring(0,200)}`}</p>
+        <button onClick={() => setReadMore(!readMore)}>
+          {readMore ? "show less" : "show more"}
+        </button>
+        <button className="delete-btn" type="button" onClick={() => removeTour(id)}>Not Interested</button>
+    </article>
+    )
 };
 
 export default Tour;
